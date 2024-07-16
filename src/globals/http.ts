105ad59bb448914ea5bb6e42bc76770c08220fca
canthugeverycat <dejanskorupan@gmail.com>
2024-07-16 20:15:17 +0200

    Deploy prep

--- src/globals/http.ts
+++ src/globals/http.ts
@@ -1,20 +1,15 @@
 import { MovieType } from "./types";
-
-const API_BASE_URL = 'http://localhost:3001';
+import data from "../mocks/db.json";
 
 /**
  * Obtains a list of movies from the API
  * @return  {MovieType[]}  List of movies
  */
 export const fetchMovies = async (): Promise<MovieType[]> => {
-    try {
-        const res = await fetch(`${API_BASE_URL}/movies`);
-    
-        const data: MovieType[] = await res.json();
-
-        return data;
-    } catch (e) {
-        const message = e instanceof Error ? e.message : 'An error has occured!';
-        throw new Error(message);
-    }
+  return new Promise<MovieType[]>((resolve) => {
+    setTimeout(() => {
+      const { movies } = data;
+      resolve(movies);
+    }, 1500);
+  });
 };
